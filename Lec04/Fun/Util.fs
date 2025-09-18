71be08f04669966e50659c04e7cec46f75fe71df
Tipskind <loef@itu.dk>
2025-09-18 15:58:38 +0200

    4.1 og 4.2

--- Lec04/Fun/Util.fs
+++ Lec04/Fun/Util.fs
@@ -10,3 +10,18 @@ let (!) (r: 'T ref)  = r.Value
 let (:=) (r: 'T ref) (v: 'T)  = r.Value <- v
 //let incr (r: int ref)  = r.Value <- r.Value + 1
 //let decr (r: int ref)  = r.Value <- r.Value - 1
+
+
+let rec sum n =
+   if n = 0 then 0
+   else n + sum (n-1);
+
+let rec power e =
+   if e = 0 then 1
+   else 3 * power (e-1);;
+
+// 3^11 + 3^10 + 3^9 + 3^8 + (...) + 3^1 + 3^0
+let rec sumpower e =
+   if e = 0 then 1
+   else power e + sumpower(e-1);;
+
